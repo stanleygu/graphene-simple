fe5b951598d3bf8ada7766917f0652124ffe4db7
Stanley Gu <stanleygu@gmail.com>
2014-05-28 18:15:28 +0000

    Added comments

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 angular.module('grapheneSimpleApp')
   .controller('MainCtrl', function($scope) {
+    // Array of node objects
     $scope.nodes = [{
       id: 'n1',
       x: 20,
@@ -12,11 +13,13 @@ angular.module('grapheneSimpleApp')
       y: 50
     }];
 
+    // Array of edges, each containing references to source and target nodes
     $scope.edges = [{
       source: $scope.nodes[0],
       target: $scope.nodes[1]
     }];
 
+    // Specify which variables to export to the template
     $scope.exports = {
       nodes: $scope.nodes,
       edges: $scope.edges
